688bf3b57b38e0ac79c572fc2f31f5a5fe341474
Wesley Claudino <claudinowesley11@gmail.com>
2021-06-18 18:25:37 -0300

    exercise 3 to the alura course

---
+++ java/exercises/src/exc003/TesteCaracteres.java
@@ -0,0 +1,23 @@
+package exc003;
+
+public class TesteCaracteres {
+	
+	public static void main(String[] args) {
+		
+		char letra = 'a';
+		System.out.println(letra);
+		
+		char valor = 66;
+		System.out.println(valor);
+		
+		valor = (char) (valor + 1);
+		System.out.println(valor);
+		
+		String palavra = "Curso de Java";
+		System.out.println(palavra);
+		
+		palavra = palavra + 2021;
+		System.out.println(palavra);
+		
+	}
+}
