e8e6a178219783d241785f14fe2f8aa018b0ad02
Wesley Claudino <claudinowesley11@gmail.com>
2021-06-18 18:25:30 -0300

    exercise 2 to the alura course

---
+++ java/exercises/src/exc002/TesteConversao.java
@@ -0,0 +1,11 @@
+package exc002;
+
+public class TesteConversao{
+
+    public static void main(String[] args){
+        double salario = 1270.50;
+        int valor = (int)salario;
+        System.out.println(valor);
+    }
+
+}
