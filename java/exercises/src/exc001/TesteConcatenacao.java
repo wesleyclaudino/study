fe6bd127631ecb6e917092cc0fd65e4336ce4328
Wesley Claudino <claudinowesley11@gmail.com>
2021-06-18 18:25:13 -0300

    exercise 1 to the alura course

---
+++ java/exercises/src/exc001/TesteConcatenacao.java
@@ -0,0 +1,12 @@
+package exc001;
+
+public class TesteConcatenacao{
+
+    public static void main(String[] args){
+
+    	String msg = "World";
+    	
+        System.out.println("Hello " + msg + "!!!");
+
+    }
+}
