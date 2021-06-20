3bbf487c4f16f27099495cdf9cdccede0a7facdd
Wesley Claudino <claudinowesley11@gmail.com>
2021-06-19 22:16:12 -0300

    exercise 8 of Alura course

---
+++ java/exercises/src/exc008/TesteSomatoria.java
@@ -0,0 +1,15 @@
+package exc008;
+
+public class TesteSomatoria {
+
+	public static void main(String[] args) {
+		int i=0;
+		int total=0;
+		
+		while(i <= 10) {
+			total += i;
+			System.out.println(total);
+			i++;
+		}
+	}
+}
---
+++ java/exercises/src/exc008/TesteWhile.java
@@ -0,0 +1,13 @@
+package exc008;
+
+public class TesteWhile {
+	
+	public static void main(String[] args) {
+		int i=0;
+		
+		while(i < 10) {
+			System.out.println(i);
+			i++;
+		}
+	}
+}
